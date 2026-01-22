656ed4df9fae27beb673ebc5a793e6ae2b304a0e
Mauricio <josemauriciodimaslopez24@gmail.com>
2026-01-21 18:57:16 -0600

    feat: agregue actividad terminada

---
+++ src/Calificaciones/Lista.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="alumno in alumnos" :key="alumno.id">
+      <strong>{{ alumno.nombre }}</strong>
+      <ul v-if="alumno.materias.length > 0">
+        <li v-for="materia in alumno.materias" :key="materia.id">
+          {{ materia.nombre }}
+        </li>
+      </ul>
+      <p v-else>Sin materias</p>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  alumnos: {
+    type: Array,
+    required: true
+  }
+})
+</script>
